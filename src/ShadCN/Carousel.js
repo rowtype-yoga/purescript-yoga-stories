c9e060c0ebdd2370b26dfb23c39d143cafc88e3f
Mark <mark.eibes@gmail.com>
2026-02-10 12:14:43 +0100

    Sidebar in pure PureScript with hooks, Carousel/InputOTP cleaned up

--- src/ShadCN/Carousel.js
+++ src/ShadCN/Carousel.js
@@ -1,40 +1,37 @@
 import useEmblaCarousel from "embla-carousel-react"
 import { createElement, createContext, useContext, useCallback, useEffect, useState } from "react"
 
-var CarouselContext = createContext(null)
+var Ctx = createContext(null)
 
 export function carouselProviderComp(props) {
-  var { children, ...rest } = props
-  var _a = useEmblaCarousel(rest.opts || {}), emblaRef = _a[0], emblaApi = _a[1]
+  var { children } = props
+  var _a = useEmblaCarousel({}), ref = _a[0], api = _a[1]
   var _b = useState(false), canPrev = _b[0], setCanPrev = _b[1]
   var _c = useState(false), canNext = _c[0], setCanNext = _c[1]
-  var onSelect = useCallback(function(api) {
-    setCanPrev(api.canScrollPrev())
-    setCanNext(api.canScrollNext())
-  }, [])
+  var onSelect = useCallback(function(a) { setCanPrev(a.canScrollPrev()); setCanNext(a.canScrollNext()) }, [])
   useEffect(function() {
-    if (!emblaApi) return
-    onSelect(emblaApi)
-    emblaApi.on("reInit", onSelect)
-    emblaApi.on("select", onSelect)
-  }, [emblaApi, onSelect])
-  return createElement(CarouselContext.Provider, { value: { emblaRef: emblaRef, emblaApi: emblaApi, canPrev: canPrev, canNext: canNext } }, ...([].concat(children)))
+    if (!api) return
+    onSelect(api)
+    api.on("reInit", onSelect)
+    api.on("select", onSelect)
+  }, [api, onSelect])
+  return createElement(Ctx.Provider, { value: { ref: ref, canPrev: canPrev, canNext: canNext, prev: function() { api && api.scrollPrev() }, next: function() { api && api.scrollNext() } } }, ...([].concat(children)))
 }
 
 export function carouselViewportComp(props) {
   var { children } = props
-  var ctx = useContext(CarouselContext)
-  return createElement("div", { className: "overflow-hidden", ref: ctx && ctx.emblaRef }, ...([].concat(children)))
+  var ctx = useContext(Ctx)
+  return createElement("div", { className: "overflow-hidden", ref: ctx && ctx.ref }, ...([].concat(children)))
 }
 
 export function carouselPrevComp(props) {
   var { children, ...rest } = props
-  var ctx = useContext(CarouselContext)
-  return createElement("button", { disabled: ctx && !ctx.canPrev, onClick: function() { ctx && ctx.emblaApi && ctx.emblaApi.scrollPrev() }, ...rest }, ...([].concat(children)))
+  var ctx = useContext(Ctx)
+  return createElement("button", { disabled: ctx && !ctx.canPrev, onClick: function() { ctx && ctx.prev() }, ...rest }, ...([].concat(children)))
 }
 
 export function carouselNextComp(props) {
   var { children, ...rest } = props
-  var ctx = useContext(CarouselContext)
-  return createElement("button", { disabled: ctx && !ctx.canNext, onClick: function() { ctx && ctx.emblaApi && ctx.emblaApi.scrollNext() }, ...rest }, ...([].concat(children)))
+  var ctx = useContext(Ctx)
+  return createElement("button", { disabled: ctx && !ctx.canNext, onClick: function() { ctx && ctx.next() }, ...rest }, ...([].concat(children)))
 }
