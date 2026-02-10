de91899ef1be9b190cef2fcf53dc22487df74cb1
Mark <mark.eibes@gmail.com>
2026-02-10 12:41:40 +0100

    InputOTP: useContext in PureScript, minimal JS re-exports only

--- src/ShadCN/Carousel.js
+++ src/ShadCN/Carousel.js
@@ -1,37 +1,13 @@
-import useEmblaCarousel from "embla-carousel-react"
-import { createElement, createContext, useContext, useCallback, useEffect, useState } from "react"
+import useEmblaCarousel from "embla-carousel-react";
 
-var Ctx = createContext(null)
+export const useEmblaCarouselImpl = () => {
+  const [ref, api] = useEmblaCarousel({});
+  return { ref, api };
+};
 
-export function carouselProviderComp(props) {
-  var { children } = props
-  var _a = useEmblaCarousel({}), ref = _a[0], api = _a[1]
-  var _b = useState(false), canPrev = _b[0], setCanPrev = _b[1]
-  var _c = useState(false), canNext = _c[0], setCanNext = _c[1]
-  var onSelect = useCallback(function(a) { setCanPrev(a.canScrollPrev()); setCanNext(a.canScrollNext()) }, [])
-  useEffect(function() {
-    if (!api) return
-    onSelect(api)
-    api.on("reInit", onSelect)
-    api.on("select", onSelect)
-  }, [api, onSelect])
-  return createElement(Ctx.Provider, { value: { ref: ref, canPrev: canPrev, canNext: canNext, prev: function() { api && api.scrollPrev() }, next: function() { api && api.scrollNext() } } }, ...([].concat(children)))
-}
-
-export function carouselViewportComp(props) {
-  var { children } = props
-  var ctx = useContext(Ctx)
-  return createElement("div", { className: "overflow-hidden", ref: ctx && ctx.ref }, ...([].concat(children)))
-}
-
-export function carouselPrevComp(props) {
-  var { children, ...rest } = props
-  var ctx = useContext(Ctx)
-  return createElement("button", { disabled: ctx && !ctx.canPrev, onClick: function() { ctx && ctx.prev() }, ...rest }, ...([].concat(children)))
-}
-
-export function carouselNextComp(props) {
-  var { children, ...rest } = props
-  var ctx = useContext(Ctx)
-  return createElement("button", { disabled: ctx && !ctx.canNext, onClick: function() { ctx && ctx.next() }, ...rest }, ...([].concat(children)))
-}
+export const scrollPrevImpl = (api) => () => api.scrollPrev();
+export const scrollNextImpl = (api) => () => api.scrollNext();
+export const canScrollPrevImpl = (api) => api.canScrollPrev();
+export const canScrollNextImpl = (api) => api.canScrollNext();
+export const onImpl = (event) => (handler) => (api) => () =>
+  api.on(event, (a) => handler(a)());
--- src/ShadCN/Carousel.purs
+++ src/ShadCN/Carousel.purs
@@ -1,28 +1,106 @@
 module ShadCN.Carousel where
 
-import React.Basic (JSX, ReactComponent)
-import Yoga.React.DOM.HTML (div)
-import Yoga.React.DOM.Internal (class IsJSX, createElement)
+import Prelude hiding (div)
+import Data.Maybe (Maybe(..))
+import Data.Nullable (Nullable, toMaybe)
+import Data.Tuple.Nested ((/\))
+import Effect (Effect)
+import Effect.Unsafe (unsafePerformEffect)
+import React.Basic (JSX, ReactContext, Ref, createContext, provider)
+import React.Basic.Events (handler_)
+import React.Basic.Hooks as React
+import Unsafe.Coerce (unsafeCoerce)
+import Web.DOM.Internal.Types (Node)
+import Yoga.React (component)
+import Yoga.React.DOM.HTML (button, div)
+import Yoga.React.DOM.Internal (class IsJSX)
 
-foreign import carouselProviderComp :: forall r. ReactComponent { | r }
-foreign import carouselViewportComp :: forall r. ReactComponent { | r }
-foreign import carouselPrevComp :: forall r. ReactComponent { | r }
-foreign import carouselNextComp :: forall r. ReactComponent { | r }
+foreign import useEmblaCarouselImpl :: Effect { ref :: Ref (Nullable Node), api :: Nullable EmblaApi }
+foreign import data EmblaApi :: Type
+foreign import scrollPrevImpl :: EmblaApi -> Effect Unit
+foreign import scrollNextImpl :: EmblaApi -> Effect Unit
+foreign import canScrollPrevImpl :: EmblaApi -> Boolean
+foreign import canScrollNextImpl :: EmblaApi -> Boolean
+foreign import onImpl :: String -> (EmblaApi -> Effect Unit) -> EmblaApi -> Effect Unit
+
+type CarouselCtx =
+  { ref :: Ref (Nullable Node)
+  , canPrev :: Boolean
+  , canNext :: Boolean
+  , prev :: Effect Unit
+  , next :: Effect Unit
+  }
+
+carouselContext :: ReactContext CarouselCtx
+carouselContext = unsafePerformEffect do
+  createContext { ref: unsafeCoerce unit, canPrev: false, canNext: false, prev: pure unit, next: pure unit }
 
 carousel :: forall kids. IsJSX kids => kids -> JSX
-carousel kids = createElement carouselProviderComp {}
-  [ div { className: "relative", role: "region" }
-    [ carouselViewport [ div { className: "flex" } kids ] ]
-  ]
+carousel kids = carouselProviderComponent
+  { children:
+      [ div { className: "relative", role: "region" }
+          [ carouselViewportComponent { children: [ div { className: "flex" } kids ] }
+          ]
+      ]
+  }
 
-carouselViewport :: forall kids. IsJSX kids => kids -> JSX
-carouselViewport = createElement carouselViewportComp {}
+carouselProviderComponent :: { children :: Array JSX } -> JSX
+carouselProviderComponent = component "CarouselProvider" \props -> React.do
+  { ref, api: rawApi } <- React.unsafeRenderEffect useEmblaCarouselImpl
+  let mbApi = toMaybe rawApi
+  canPrev /\ setCanPrev <- React.useState' false
+  canNext /\ setCanNext <- React.useState' false
+  let
+    onSelect a = do
+      setCanPrev (canScrollPrevImpl a)
+      setCanNext (canScrollNextImpl a)
+  React.useEffectAlways do
+    case mbApi of
+      Nothing -> pure (pure unit)
+      Just api -> do
+        onSelect api
+        onImpl "reInit" onSelect api
+        onImpl "select" onSelect api
+        pure (pure unit)
+  let
+    prev = case mbApi of
+      Nothing -> pure unit
+      Just api -> scrollPrevImpl api
+    next = case mbApi of
+      Nothing -> pure unit
+      Just api -> scrollNextImpl api
+  pure $ provider carouselContext { ref, canPrev, canNext, prev, next } props.children
+
+carouselViewportComponent :: { children :: Array JSX } -> JSX
+carouselViewportComponent = component "CarouselViewport" \props -> React.do
+  ctx <- React.useContext carouselContext
+  pure $ div { className: "overflow-hidden", ref: ctx.ref } props.children
 
 carouselItem :: forall kids. IsJSX kids => kids -> JSX
 carouselItem = div { className: "min-w-0 shrink-0 grow-0 basis-full", role: "group" }
 
-carouselPrevious :: forall kids. IsJSX kids => kids -> JSX
-carouselPrevious = createElement carouselPrevComp { className: "absolute z-10 flex size-8 items-center justify-center rounded-full border bg-background shadow-sm disabled:opacity-50 -left-12 top-1/2 -translate-y-1/2" }
+carouselPrevious :: Array JSX -> JSX
+carouselPrevious kids = carouselPreviousComponent { children: kids }
 
-carouselNext :: forall kids. IsJSX kids => kids -> JSX
-carouselNext = createElement carouselNextComp { className: "absolute z-10 flex size-8 items-center justify-center rounded-full border bg-background shadow-sm disabled:opacity-50 -right-12 top-1/2 -translate-y-1/2" }
+carouselPreviousComponent :: { children :: Array JSX } -> JSX
+carouselPreviousComponent = component "CarouselPrevious" \props -> React.do
+  ctx <- React.useContext carouselContext
+  pure $ button
+    { className: "absolute z-10 flex size-8 items-center justify-center rounded-full border bg-background shadow-sm disabled:opacity-50 -left-12 top-1/2 -translate-y-1/2"
+    , disabled: not ctx.canPrev
+    , onClick: handler_ ctx.prev
+    }
+    props.children
+
+carouselNext :: Array JSX -> JSX
+carouselNext kids = carouselNextComponent { children: kids }
+
+carouselNextComponent :: { children :: Array JSX } -> JSX
+carouselNextComponent = component "CarouselNext" \props -> React.do
+  ctx <- React.useContext carouselContext
+  pure $ button
+    { className: "absolute z-10 flex size-8 items-center justify-center rounded-full border bg-background shadow-sm disabled:opacity-50 -right-12 top-1/2 -translate-y-1/2"
+    , disabled: not ctx.canNext
+    , onClick: handler_ ctx.next
+    }
+    props.children
