76d227185d85f6166257b2a835a35a8e5fa91653
Mark <mark.eibes@gmail.com>
2026-02-10 12:38:48 +0100

    Use const for all JS exports

--- src/ShadCN/InputOTP.js
+++ src/ShadCN/InputOTP.js
@@ -1,11 +1,4 @@
-import { OTPInput, OTPInputContext } from "input-otp"
-import { createElement, useContext } from "react"
+import { OTPInput, OTPInputContext } from "input-otp";
 
-export const otpInput = OTPInput
-
-export function otpSlotComp(props) {
-  var { index, ...rest } = props
-  var ctx = useContext(OTPInputContext)
-  var slot = ctx && ctx.slots ? ctx.slots[index] : {}
-  return createElement("div", { "data-active": slot.isActive || false, ...rest }, slot.char || "")
-}
+export const otpInput = OTPInput;
+export const otpInputContext = OTPInputContext;
--- src/ShadCN/InputOTP.purs
+++ src/ShadCN/InputOTP.purs
@@ -1,20 +1,37 @@
 module ShadCN.InputOTP where
 
-import React.Basic (JSX, ReactComponent)
+import Prelude
+import Data.Array (index)
+import Data.Maybe (fromMaybe)
+import Data.Nullable (Nullable, null, toMaybe)
+import React.Basic (JSX, ReactComponent, ReactContext)
+import React.Basic.Hooks as React
+import Yoga.React (component)
 import Yoga.React.DOM.HTML (div)
 import Yoga.React.DOM.Internal (class IsJSX, createElement, text)
 
 foreign import otpInput :: forall r. ReactComponent { | r }
-foreign import otpSlotComp :: forall r. ReactComponent { | r }
+foreign import otpInputContext :: ReactContext { slots :: Array { char :: Nullable String, isActive :: Boolean } }
 
 inputOTP :: forall r. { | r } -> JSX
-inputOTP props = createElement otpInput { containerClassName: "flex items-center gap-2" } ([] :: Array JSX)
+inputOTP _ = createElement otpInput { containerClassName: "flex items-center gap-2" } ([] :: Array JSX)
 
 inputOTPGroup :: forall kids. IsJSX kids => kids -> JSX
 inputOTPGroup = div { className: "flex items-center" }
 
 inputOTPSlot :: Int -> JSX
-inputOTPSlot index = createElement otpSlotComp { index, className: "data-[active=true]:border-ring data-[active=true]:ring-ring/50 border-input relative flex h-9 w-9 items-center justify-center border-y border-r text-sm shadow-xs transition-all outline-none first:rounded-l-md first:border-l last:rounded-r-md data-[active=true]:z-10 data-[active=true]:ring-[3px]" } ([] :: Array JSX)
+inputOTPSlot i = inputOTPSlotComponent { index: i }
+
+inputOTPSlotComponent :: { index :: Int } -> JSX
+inputOTPSlotComponent = component "InputOTPSlot" \props -> React.do
+  ctx <- React.useContext otpInputContext
+  let slot = fromMaybe { char: null, isActive: false } (index ctx.slots props.index)
+  let char = fromMaybe "" (toMaybe slot.char)
+  pure $ div
+    { className: "data-[active=true]:border-ring data-[active=true]:ring-ring/50 border-input relative flex h-9 w-9 items-center justify-center border-y border-r text-sm shadow-xs transition-all outline-none first:rounded-l-md first:border-l last:rounded-r-md data-[active=true]:z-10 data-[active=true]:ring-[3px]"
+    , "data-active": show slot.isActive
+    }
+    (text char)
 
 inputOTPSeparator :: JSX
 inputOTPSeparator = div { role: "separator" } (text "-")
